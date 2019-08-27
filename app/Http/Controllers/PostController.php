2121477e7cd35f003914cf900aa7c1cb8bd726f2
ayethandar <ayethandar@management-partners.co.jp>
2019-08-27 08:12:31 +0630

    merge

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,8 +15,10 @@ class PostController extends Controller
     public function index()
     {
         $news_list = Post::all()->toArray();
-        $data = array("news_list" => $news_list);
-       return response()->json($data);
+        // $data = array("news_list" => $news_list);
+       return response()->json(array_reverse($news_list));
+    //    $news_list = Post::all()->toArray();
+    //     return array_reverse($news_list);
     }
     // add news
     public function add(Request $request)
@@ -59,7 +61,7 @@ class PostController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * Display the specified resource.  
      *
      * @param  \App\Post  $post
      * @return \Illuminate\Http\Response
