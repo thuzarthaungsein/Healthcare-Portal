22cfa4a0fdbd56ec8c31f6d900280bd966b4f3a6
ThuzarTS <thuzar.ts92@gmail.com>
2019-08-29 11:19:12 +0630

    upload folder

--- database/migrations/2019_08_20_085846_create_hospital_profile_table.php
+++ database/migrations/2019_08_20_085846_create_hospital_profile_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCustomerProfileTable extends Migration
+class CreateHospitalProfileTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCustomerProfileTable extends Migration
      */
     public function up()
     {
-        Schema::create('customer_profile', function (Blueprint $table) {
+        Schema::create('hospital_profile', function (Blueprint $table) {
             $table->increments('id');
             $table->text('access')->nullable();
             $table->text('medical_department')->nullable();
@@ -35,6 +35,6 @@ class CreateCustomerProfileTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('customer_profile');
+        Schema::dropIfExists('hospital_profile');
     }
 }
