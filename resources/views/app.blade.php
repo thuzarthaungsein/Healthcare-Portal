15bbeb435a53e3feed85a833a0a64f83d1a34851
YanLynn <yan.lynn1991@gmail.com>
2019-09-23 13:13:34 +0630

    admin

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,62 +19,32 @@
 
 
 <!-- Scripts -->
-
 <script src="{{ asset('js/app.js') }}" defer></script>
-
 <script src="{{ asset('js/myJs.js') }}" defer></script>
-
-
-
 <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.8/css/select2.min.css" rel="stylesheet" />
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.8/js/select2.min.js"></script> -->
-
 <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.13/vue.js"></script>
 <!-- Include after Vue (before closing body) -->
 <script src="https://unpkg.com/vue-fullpage.js/dist/vue-fullpage.min.js"></script>
-
 <!-- Fonts -->
-
 <!-- <link rel="dns-prefetch" href="//fonts.gstatic.com"> -->
 <!--mailbox-->
-
-
-
 <!-- Styles -->
-
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
 <link href="{{ asset('css/mystyle.css') }}" rel="stylesheet">
-
 <link href="{{ asset('css/all.css') }}" rel="stylesheet">
-
 <link href="{{ asset('css/jquery.scrolling-tabs.min.css') }}" rel="stylesheet">
-
 <link rel="stylesheet" href="https://unpkg.com/fullpage.js/dist/fullpage.min.css">
-
-
-
 <!-- <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css"> -->
-
     <style>
-
         .bg-light {
-
             background-color: #eae9e9 !important;
-
         }
-
     </style>
-
 <!-- link for editor -->
-
 <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-bs4.css" rel="stylesheet">
-
 <!-- end link for editor -->
-
 </head>
 
 <body>
@@ -735,32 +705,6 @@
 
     $('.DataTable').DataTable();
     var csrf = "{{ csrf_token() }}";
-
-    // $.ajax({
-
-    //     url: '/api/get_latest_post_all_cat',
-
-    //     type: 'GET',
-
-    //     data: {'_token': csrf},
-
-
-
-    //     success: function( data ) {
-
-
-
-    //         for (var i = 0; i < data.length; i++) {
-    //             var photo = '<li class="list-group-item adslist-card"><a href="/newsdetails/'+data[i].id+'"><img class="img-responsivie ads-img" src="../upload/news/' + data[i].photo + '" />';
-    //             var title = '<h3 class="smallads-title">' + data[i].title + '</h3></li>';
-
-    //             $("#menu").append(photo + title);
-
-    //         }
-
-    //     }
-
-    // });
 
     $.ajax({
         url: '/api/advertisement/ads',
