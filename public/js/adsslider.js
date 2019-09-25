8feac4f8fb509e56bdac000e1f773907d9c10890
EiM0hM0hAung <eimoh.aidma@gmail.com>
2019-09-25 09:51:21 +0630

    map

--- public/js/adsslider.js
+++ public/js/adsslider.js
@@ -1,15 +1,14 @@
-jssor_slider2_init = function () {
+jssor_slider2_init = function() {
     //Reference https://www.jssor.com/development/tip-make-responsive-slider.html
     var options = {
-        $AutoPlay: 1,           //[Optional] Auto play or not, to enable slideshow, this option must be set to greater than 0. Default value is 0. 0: no auto play, 1: continuously, 2: stop at last slide, 4: stop on click, 8: stop on user navigation (by arrow/bullet/thumbnail/drag/arrow key navigation)
-        $DragOrientation: 3     //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $Cols is greater than 1, or parking position is not 0),
+        $AutoPlay: 1, //[Optional] Auto play or not, to enable slideshow, this option must be set to greater than 0. Default value is 0. 0: no auto play, 1: continuously, 2: stop at last slide, 4: stop on click, 8: stop on user navigation (by arrow/bullet/thumbnail/drag/arrow key navigation)
+        $DragOrientation: 3 //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $Cols is greater than 1, or parking position is not 0),
     };
 
-    var jssor_slider2 = new $JssorSlider$('slider2_container', options);
+    var jssor_slider2 = new $JssorSlider$("slider2_container", options);
     /*#region responsive code begin*/
     //you can remove responsive code if you don't want the slider scales while window resizing
     function ScaleSlider() {
-
         //reserve blank width for margin+padding: margin+padding-left (10) + margin+padding-right (10)
         var paddingWidth = 20;
 
@@ -22,7 +21,6 @@ jssor_slider2_init = function () {
         var parentWidth = parentElement.clientWidth;
 
         if (parentWidth) {
-
             //exclude blank width
             var availableWidth = parentWidth - paddingWidth;
 
@@ -38,7 +36,6 @@ jssor_slider2_init = function () {
 
             //evaluate free width for text, if the width is less than minReserveWidth then fill parent container
             if (availableWidth - sliderWidth < minReserveWidth) {
-
                 //set slider width to available width
                 sliderWidth = availableWidth;
 
@@ -53,9 +50,7 @@ jssor_slider2_init = function () {
             toClearElment && (toClearElment.style["clear"] = clearFix);
 
             jssor_slider2.$ScaleWidth(sliderWidth);
-        }
-        else
-            $Jssor$.$Delay(ScaleSlider, 30);
+        } else $Jssor$.$Delay(ScaleSlider, 30);
     }
 
     ScaleSlider();
