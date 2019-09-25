ba6369ba34e0b917bec35ed63aac383b6bea6d8b
EiM0hM0hAung <eimoh.aidma@gmail.com>
2019-09-25 14:23:51 +0630

    no record data

--- resources/assets/js/components/JobOfferList.vue
+++ resources/assets/js/components/JobOfferList.vue
@@ -18,40 +18,43 @@
           <!-- <p style="font-size:30px;color:#69C1A8;font-weight:bold;margin:20px 0;">
             <span style="font-size:50px;">NO</span>
             <span style="color:#769BD0;">RECORD</span> DATA
-          </p>
-          <p>表示するデータありません‼新しいデータを作成してください。</p>         
-          <router-link to= '/joboffercreate' class="float-right main-bg-color create-btn all-btn" style="width: 16%;margin: 30px auto;">
-          <i class="fas fa-plus-circle"></i> 新しいデータ作成
-          </router-link>
+          </p>-->
+          <p class="record-txt01">表示するデータありません</p>
+          <p>表示するデータありません‼新しいデータを作成してください。</p>
+          <a href="/joboffercreate" class="main-bg-color create-btn all-btn">
+            <i class="fas fa-plus-circle"></i> 新しいデータ作成
+          </a>
         </div>
 
         <div v-else v-for="job in jobs" :key="job.id" class="card card-default m-b-20">
           <div class="card-body news-post">
             <div class="row">
-              <div class="col-md-2">
+              <!-- <div class="col-md-2">
                                                <img  :src="'images/'+ (job.location)" class="col-md-12" alt="" />
-          </div>-->
-          <div class="col-md-10">
-            <div class="col-sm-8 pad-free mb-2">
-              <b>
-                <router-link
-                  :to="{name: 'job_details', params:{id:job.id}}"
-                  class="mr-auto"
-                >{{job.title}}</router-link>
-                <!-- <a href="../jobs/job_search_details.html" class="mr-auto">{{job.title}} </a> -->
-              </b>
-            </div>
-            <p>{{job.description}}</p>
-            <p>{{job.working_hours}}</p>
-            <div class="row">
-              <div class="col-sm-4">
-                <router-link
-                  :to="{name: 'joboffercreate', params:{id:job.id}}"
-                  class="btn main-bg-color white all-btn"
-                >Edit</router-link>
-                <button class="btn btn-danger all-btn" @click="deleteJob(job.id)">Delete</button>
-                <!-- <router-link :to="{name: 'job_details', params:{id:job.id}}" class="btn btn all-btn secondary-bg-color white">Delete</router-link> -->
-                <!-- <a href="../jobs/job_search_details.html"  class="btn btn all-btn secondary-bg-color white">詳細を見る</a> -->
+              </div>-->
+              <div class="col-md-10">
+                <div class="col-sm-8 pad-free mb-2">
+                  <b>
+                    <router-link
+                      :to="{name: 'job_details', params:{id:job.id}}"
+                      class="mr-auto"
+                    >{{job.title}}</router-link>
+                    <!-- <a href="../jobs/job_search_details.html" class="mr-auto">{{job.title}} </a> -->
+                  </b>
+                </div>
+                <p>{{job.description}}</p>
+                <p>{{job.working_hours}}</p>
+                <div class="row">
+                  <div class="col-sm-4">
+                    <router-link
+                      :to="{name: 'joboffercreate', params:{id:job.id}}"
+                      class="btn main-bg-color white all-btn"
+                    >Edit</router-link>
+                    <button class="btn btn-danger all-btn" @click="deleteJob(job.id)">Delete</button>
+                    <!-- <router-link :to="{name: 'job_details', params:{id:job.id}}" class="btn btn all-btn secondary-bg-color white">Delete</router-link> -->
+                    <!-- <a href="../jobs/job_search_details.html"  class="btn btn all-btn secondary-bg-color white">詳細を見る</a> -->
+                  </div>
+                </div>
               </div>
             </div>
           </div>
