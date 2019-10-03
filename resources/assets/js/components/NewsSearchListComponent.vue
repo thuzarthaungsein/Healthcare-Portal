4e29f93c62fb916e90b885e90f0cc3038501c95d
chhuani <lalhmachhuani261994@gmail.com>
2019-10-03 14:41:33 +0630

    CN-NewsSearchListComponent

--- resources/assets/js/components/NewsSearchListComponent.vue
+++ resources/assets/js/components/NewsSearchListComponent.vue
@@ -1,5 +1,4 @@
 <template>
-    <div class="row">
         <div class="col-sm-12 pad-free">
           <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
@@ -9,7 +8,6 @@
               <li class="breadcrumb-item active" aria-current="page">新しい検索</li>
             </ol>
           </nav>
-        </div>
 
         <div class="row">
             <div class="col-12">
@@ -17,20 +15,20 @@
                     <div class="col-md-12 category_box"  v-for="(group,index) in post_groups" :key="index">
                         <h4 class="category_news_title"><span>{{group.name}}</span></h4>
                             <div class="row m-lr-0">
-                                    <div class="col-md-3 p-l-0" v-for="item in posts" :key="item.id" v-if="item.cat_id == group.cat_id">
-                                        <div class="col-md-12 row m-b-10 adslist-card m-lr-0" style="background-color: #eee;box-shadow: 0 0 2px #bfb9b9;">
-                                            <div class="col-md-4 img-box">
-                                                <router-link :to="'/newsdetails/'+item.id">
-                                                    <img v-bind:src="'/upload/news/' + item.photo" class="fit-image" style="height:5rem;width:6rem" @error="imgUrlAlt">
-                                                </router-link>
-                                            </div>
-                                            <div class="col-md-8 txt-box">
-                                                <router-link :to="'/newsdetails/'+item.id">
-                                                    <p class="news-title smallads-title"> {{item.title}} </p>
-                                                </router-link>
-                                            </div>
+                                <div class="col-md-3 p-l-0" v-for="item in posts" :key="item.id" v-if="item.cat_id == group.cat_id">
+                                    <div class="col-md-12 row m-b-10 adslist-card m-lr-0" style="background-color: #eee;box-shadow: 0 0 2px #bfb9b9;">
+                                        <div class="col-md-4 img-box">
+                                            <router-link :to="'/newsdetails/'+item.id">
+                                                <img v-bind:src="'/upload/news/' + item.photo" class="fit-image" style="height:5rem;width:6rem" @error="imgUrlAlt">
+                                            </router-link>
+                                        </div>
+                                        <div class="col-md-8 txt-box">
+                                            <router-link :to="'/newsdetails/'+item.id">
+                                                <p class="news-title smallads-title"> {{item.title}} </p>
+                                            </router-link>
                                         </div>
                                     </div>
+                                </div>
                             </div>                             
                         </div>
                     </div>
