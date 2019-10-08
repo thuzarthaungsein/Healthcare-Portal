03b9f934ef8ae46d2afc31d7933b124ace7f893f
May <mayphuekyawsoe123@gmail.com>
2019-10-08 09:55:19 +0630

    merge with job

--- resources/assets/js/components/Subject.vue
+++ resources/assets/js/components/Subject.vue
@@ -9,7 +9,7 @@
                     <div class="card-body">
                         <div class="row">
                          <div class="col-sm-1"></div>
-                         <div class="col-sm-9">  
+                         <div class="col-sm-9">
                                 <form @submit.prevent ="add" class="m-t-16">
                                         <div class="form-group row">
                                             <div class="col-sm-3 text-right">
@@ -40,14 +40,9 @@
                                                     </select>
                                                 </div>
                                         </div>
-
-                                          <div class="form-group row">
-                                                <div class="col-sm-10">
-
-                                                </div>
-                                                <div class="col-sm-2">
-                                                     <button class="btn news-post-btn">Create</button>
-                                                </div>
+                                        <div class="form-group">
+                                                <router-link class="btn btn-danger all-btn" to="/subjectlist" > キャンセル </router-link>
+                                                <button class="btn news-post-btn">作成</button>
                                         </div>
                                 </form>
                            </div>
@@ -92,10 +87,10 @@ export default {
               }.bind(this));
         },
         mounted() {
-           
+
             if(this.$route.params.id)
             {
-              
+
                 this.axios
                .get(`/api/subjects/edit/${this.$route.params.id}`)
                 .then((response) => {
@@ -105,15 +100,15 @@ export default {
                         this.SubjectList.name = response.data.name;
                 });
             }
-             
+
 
         },
 
          methods: {
             add() {
                 if(this.$route.params.id)
-                {   
-                       this.updateSubject();    
+                {
+                       this.updateSubject();
                 }
                 else{
                   this.axios.post('/api/subjects/add', this.Subject)
@@ -137,7 +132,7 @@ export default {
 
            },
            updateSubject() {
-             
+
 
                 this.axios
                     .post(`api/subjects/update/${this.$route.params.id}`, this.Subject)
