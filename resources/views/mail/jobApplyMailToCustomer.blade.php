ddf0babaf8993481f6dda0194f93478e2070ab20
sawnwaiyan <sawnwaiyan2011@gmail.com>
2019-10-10 14:57:03 +0630

    job apply mail to user,customer, admin

--- resources/views/mail/jobApplyMailToCustomer.blade.php
+++ resources/views/mail/jobApplyMailToCustomer.blade.php
@@ -13,5 +13,13 @@
     実行可能日 : {{$getComment->workable_day}}  <br>
     リマーク  : {{$getComment->remark}}  <br>
 
+    Job Description
+    job_title : {{$getComment->job_title}}  <br>
+    job_description: {{$getComment->job_description}} <br>
+    job_location: {{$getComment->job_location}} <br>
+    job_nearest_station: {{$getComment->job_nearest_station}} <br>
+    job_employment_status: {{$getComment->job_employment_status}} <br>
+    job_salary: {{$getComment->job_salary}} <br>
+    job_working_hours: {{$getComment->job_working_hours}} <br>
 
 </div>
