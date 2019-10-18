d8fff7fa2a9db7396367be81bcfcfc34e7bcb80d
ThuzarTS <thuzar.ts92@gmail.com>
2019-10-18 14:50:16 +0630

    Profile moving in and permonth bind

--- resources/assets/js/components/ProfilePublish.vue
+++ resources/assets/js/components/ProfilePublish.vue
@@ -148,46 +148,53 @@
                             <table class="table table-bordered">
                                     <tbody>
                                         <tr>
-                                        <th width="250" class="custom-bg-color">
-                                            <font>住所</font>
-                                        </th>
-                                        <td>
-                                            <font>{{cust.address}}</font>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th width="250" class="custom-bg-color">
-                                            <font>電話</font>
-                                        </th>
-                                        <td>
-                                            <font>{{cust.phone}}</font>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th width="250" class="custom-bg-color">
-                                            <font>アクセス　</font>
-                                        </th>
-                                        <td>
-                                            <font>{{cust.access}}</font>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th width="250" class="custom-bg-color">
-                                            <font>駅</font>
-                                        </th>
-                                        <td>
-                                            <font>Nearest Station</font>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th width="250" class="custom-bg-color">
-                                            <font>費用 </font>
-                                        </th>
-                                        <td>
-                                            <font>expense</font>
-                                        </td>
-                                    </tr>
-                                    
+                                            <th width="250" class="custom-bg-color">
+                                                <font>入居時</font>
+                                            </th>
+                                            <td>
+                                                <font>{{cust.moving_in}}</font>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th width="250" class="custom-bg-color">
+                                                <font>月額</font>
+                                            </th>
+                                            <td>
+                                                <font>{{cust.per_month}}</font>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th width="250" class="custom-bg-color">
+                                                <font>住所</font>
+                                            </th>
+                                            <td>
+                                                <font>{{cust.address}}</font>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th width="250" class="custom-bg-color">
+                                                <font>電話</font>
+                                            </th>
+                                            <td>
+                                                <font>{{cust.phone}}</font>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th width="250" class="custom-bg-color">
+                                                <font>アクセス　</font>
+                                            </th>
+                                            <td>
+                                                <font>{{cust.access}}</font>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th width="250" class="custom-bg-color">
+                                                <font>駅</font>
+                                            </th>
+                                            <td>
+                                                <font>Nearest Station</font>
+                                            </td>
+                                        </tr>
                                     </tbody>
                             </table>
 
@@ -1547,7 +1554,7 @@ $(document).scroll(function() {
 
 
                   this.axios.get('/api/profile/customer/'+this.cusid+'/'+this.type) .then(response => {
-                      console.log(response.data);
+                      
                       this.customer = response.data;
 
                 });
