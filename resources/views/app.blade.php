6d9b0b6e48b07defcf75b116c5c623296e3f59e2
EiM0hM0hAung <eimoh.aidma@gmail.com>
2019-10-22 08:23:11 +0630

    ads slider height adjust

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -592,7 +592,7 @@
                                 @if(Auth::check())
                                 <div class="row justify-content-md-center p-t-15">
                                 @else
-                                <div class="row justify-content-md-center p-t-130">
+                                <div class="row justify-content-md-center p-t-110">
                                 
                                 @endif
 
