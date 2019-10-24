d2a552b54b2caf79249b4f45938c0e0e1bf502b3
ThuzarTS <thuzar.ts92@gmail.com>
2019-10-24 16:51:37 +0630

    Home button fixed link and display check

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -90,16 +90,23 @@
             border: none;
             outline: none;
             background-color: #ff6117;
-            color: #fff;
+            color: #fff!important;
             cursor: pointer;
             border: 1px solid #e66527;
-            border-radius: 30px;
-            border-top-left-radius: 0px;
-            border-bottom-left-radius: 0px;
+            border-radius: 30px !important;
+            border-top-left-radius: 0px !important;
+            border-bottom-left-radius: 0px !important;
             box-sizing: border-box;
             padding: 10px 20px;
             /* background: url(../images/home-24.png) no-repeat center; */
             box-shadow: 0 9px 10px -10px #d2571c;
+            text-decoration: none;
+        }
+        #homeBtn i.fa {
+            color: white!important;
+        }
+        #homeBtn:hover {
+            background: #fbaa84;
         }
     </style>
 <!-- link for editor -->
@@ -109,9 +116,11 @@
 
 <body>
     <button onclick="topFunction()" id="myBtn">Top</button>
-    <button id="homeBtn"><i class="fa fa-home"></i> ホーム</button>
+    
 <div id="app">
-
+@if(!Auth::check())
+    <router-link to="/" id="homeBtn"><i class="fa fa-home"></i> ホーム </router-link>
+@endif
     <div class="main-content pad-free">
 
         <flash message=""></flash>
