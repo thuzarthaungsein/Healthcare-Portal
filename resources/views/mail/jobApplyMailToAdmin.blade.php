5078de6cfdcfa05893d0005e6b19e7a1faf81888
sus547983 <susandiaung565@gmail.com>
2019-10-25 09:40:32 +0630

    jobapply mail template

--- resources/views/mail/jobApplyMailToAdmin.blade.php
+++ resources/views/mail/jobApplyMailToAdmin.blade.php
@@ -1,27 +1,21 @@
-    <!-- <div>
-        Dear admin,
-        job_title : {{$getComment->job_title}}  <br>
-
-        Successfully applied!
-    </div> -->
 
     <pre>
-        [施設名」([施設No])様
+        [{{$getComment->cus_name}} 」([施設No])様
 
         [サイト名]より求人への応募がありました。
 
 
-        【求人タイトル】
+        【{{$getComment->job_title}}】
         xxxxxx
-        [求人番号]
+        [{{$getComment->jobnum}}]
 
         【ご応募された方の情報】
         お名前：{{$getComment->first_name}}
-        ふりがな：
+        ふりがな：{{$getComment->last_name}}
         生年月日：{{$getComment->birthday}}
         性別：{{$getComment->gender}}
         郵便番号：{{$getComment->postal}}
-        都道府県：
+        都道府県：{{$getComment->city_name}}
         市区町村、番地（建物名)：{{$getComment->home_address}}
         電話番号：{{$getComment->phone}}
         メールアドレス：{{$getComment->email}}
@@ -39,27 +33,4 @@
 
 
     
-<div>
-    Dear admin,
-    Job Description
-    job_title : {{$getComment->job_title}}  <br>
-    job_description: {{$getComment->job_description}} <br>
-    job_location: {{$getComment->job_location}} <br>
-    job_nearest_station: {{$getComment->job_nearest_station}} <br>
-    job_employment_status: {{$getComment->job_employment_status}} <br>
-    job_salary: {{$getComment->job_salary}} <br>
-    job_working_hours: {{$getComment->job_working_hours}} <br>
 
-    お名前 : {{$getComment->first_name}}  <br>
-    お名前フリガナ : {{$getComment->last_name}}
-    生年月日 : {{$getComment->birthday}}  <br>
-    性別  : {{$getComment->gender}}  <br>
-    郵便番号 : {{$getComment->postal}}  <br>
-    住所 : {{$getComment->str_address}}  <br>
-    番地以下 : {{$getComment->home_address}}  <br>
-    電話番号 : {{$getComment->phone}}  <br>
-    メールアドレス : {{$getComment->email}}  <br>
-    資格 : {{$getComment->qualification}}  <br>
-    実行可能日 : {{$getComment->workable_day}}  <br>
-    リマーク  : {{$getComment->remark}}  <br>
-</div>
