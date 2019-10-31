ceaf967d16c394f9bd0d5aa8b6b58e59e8436049
sus547983 <susandiaung565@gmail.com>
2019-10-31 10:49:57 +0630

    nursing search

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,7 @@ Route::group(['middleware' => ['auth']], function() {
 
 // public route api start
     Route::get('gethospitalsearch','SearchMapController@getHospitalSearch');
+    Route::get('getnursingsearch','SearchMapController@getNursingSearch');
     Route::post('getmap/{id}','SearchMapController@getMap');
     Route::get('getjobsearch','SearchMapController@getJobSearch');
     Route::post('getmaptownship/{id}','SearchMapController@getMapTownship');
