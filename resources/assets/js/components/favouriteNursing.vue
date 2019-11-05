d990fa107d8fa9f3ac1090b0e6af71b34af9658f
ThuzarTS <thuzar.ts92@gmail.com>
2019-11-05 15:28:53 +0630

    svg link remove

--- resources/assets/js/components/favouriteNursing.vue
+++ resources/assets/js/components/favouriteNursing.vue
@@ -14,7 +14,7 @@
                 </div>
                 <div class="col-md-12">
                     <div class="col-md-12 fav-his-header">
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 172 172" style=" fill:#000000;">
+                        <svg x="0px" y="0px" width="24" height="24" viewBox="0 0 172 172" style=" fill:#000000;">
                             <g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
                                 <path d="M0,172v-172h172v172z" fill="none"></path>
                                 <g id="original-icon" fill="#c40000" opacity="0" visibility="hidden">
