628e62cc6409dcf23f8aa77c082eef3efe351941
mawutmoneoo <mawutmoneoo@gmail.com>
2019-11-18 16:39:19 +0630

    change jp lan in auth

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'パスワードが一致しませんでした。',
+    'throttle' => 'ログイン試行が多すぎです。後でもう一度やり直してください。',
 
 ];
