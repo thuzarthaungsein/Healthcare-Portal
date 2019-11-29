4ab74260063031b8e1812c0ca873da0c7b4dc375
EiM0hM0hAung <eimoh.aidma@gmail.com>
2019-11-29 08:17:22 +0630

    card coursel  wrapper width adjust for fatourite nursing & nursing history

--- resources/assets/js/components/NursingHistory.vue
+++ resources/assets/js/components/NursingHistory.vue
@@ -1,91 +1,93 @@
 <template>
-  <div class="col-12 scrolldiv2 pb-3 tab-content" id="nursing">
-    <div class="col-12 pl-0">
-      <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item">
-            <router-link to="/">ホーム</router-link>
-          </li>
-          <li class="breadcrumb-item active" aria-current="page">介護の歴史</li>
-        </ol>
-      </nav>
-      <div class="col-md-12">
-          <div class="col-md-12 fav-his-header">
-              <svg x="0px" y="0px" width="24" height="24" viewBox="0 0 172 172" style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#c40000"><path d="M86,15.0472l-78.83333,70.9528h21.5v64.5h59.44694c-1.3545,-4.54367 -2.11361,-9.3525 -2.11361,-14.33333h-43v-63.14225l43,-38.6888l57.61328,51.66439h21.22006zM136.19466,100.24935c-19.78717,0 -35.83333,16.04617 -35.83333,35.83333c0,19.78717 16.04617,35.83333 35.83333,35.83333c19.78717,0 35.83333,-16.04617 35.83333,-35.83333c0,-19.78717 -16.04617,-35.83333 -35.83333,-35.83333zM150.89193,119.24382l10.02213,10.03613l-28.30274,28.30274l-21.13606,-21.13607l10.02213,-10.03613l11.11393,11.11393z"></path></g></g></svg>
-        &nbsp;<span class="font-weight-bold"> 最近見た施設リスト</span>
-         &nbsp;<span class ="job_count"> {{his_nus}}件</span>
-          <!-- &nbsp;<span style="color:#000;">件</span> -->
-        </div>
-      </div>
-    </div>
+    <div class="scrolldiv2 pb-5 tab-content" id="nursing">
+        <div class="row m-0">
+            <div class="col-12 pl-0">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <router-link to="/">ホーム</router-link>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">介護の歴史</li>
+                    </ol>
+                </nav>
+            </div>
+            <div class="col-md-12">
+                <div class="col-md-12 fav-his-header">
+                    <svg x="0px" y="0px" width="24" height="24" viewBox="0 0 172 172" style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#c40000"><path d="M86,15.0472l-78.83333,70.9528h21.5v64.5h59.44694c-1.3545,-4.54367 -2.11361,-9.3525 -2.11361,-14.33333h-43v-63.14225l43,-38.6888l57.61328,51.66439h21.22006zM136.19466,100.24935c-19.78717,0 -35.83333,16.04617 -35.83333,35.83333c0,19.78717 16.04617,35.83333 35.83333,35.83333c19.78717,0 35.83333,-16.04617 35.83333,-35.83333c0,-19.78717 -16.04617,-35.83333 -35.83333,-35.83333zM150.89193,119.24382l10.02213,10.03613l-28.30274,28.30274l-21.13606,-21.13607l10.02213,-10.03613l11.11393,11.11393z"></path></g></g></svg>
+                &nbsp;<span class="font-weight-bold"> 最近見た施設リスト</span>
+                &nbsp;<span class ="job_count"> {{his_nus}}件</span>
+                <!-- &nbsp;<span style="color:#000;">件</span> -->
+                </div>
+            </div>
 
-    <!--modal-->
-       <div class="modal fade bd-example-modal-google googlecheck" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" style="display:none;">
-                        <div class="modal-dialog modal-xl" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLongTitle">交通アクセス／{{custname}}</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <button class="btn btn-secondary">閉じる&times;</button>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    <GmapMap id="googlemap" ref="map" :center="center" :zoom="10">
-                                        <GmapMarker v-for="(m, index) in markers" :key="index" :position="m.position" :clickable="true" :draggable="true" @click="center=m.position" />
-                                    </GmapMap>
-                                </div>
-                                <div class="modal-body">
-                                    <span class="job_ico"><i class="fas fa-map-marker-alt"></i></span><strong>住所</strong>
-                                    <br>
-                                    <span>{{address}}</span>
-                                    <hr>
-                                    <span class="job_ico"><i class="fa fa-map-signs"></i></span><strong>最寄り駅</strong>
-                                    <br>
-                                    <p v-html="access"></p>
-                                </div>
-                                <div class="modal-body">
+            <!--modal-->
+            <div class="modal fade bd-example-modal-google googlecheck" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" style="display:none;">
+                <div class="modal-dialog modal-xl" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle">交通アクセス／{{custname}}</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <button class="btn btn-secondary">閉じる&times;</button>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <GmapMap id="googlemap" ref="map" :center="center" :zoom="10">
+                                <GmapMarker v-for="(m, index) in markers" :key="index" :position="m.position" :clickable="true" :draggable="true" @click="center=m.position" />
+                            </GmapMap>
+                        </div>
+                        <div class="modal-body">
+                            <span class="job_ico"><i class="fas fa-map-marker-alt"></i></span><strong>住所</strong>
+                            <br>
+                            <span>{{address}}</span>
+                            <hr>
+                            <span class="job_ico"><i class="fa fa-map-signs"></i></span><strong>最寄り駅</strong>
+                            <br>
+                            <p v-html="access"></p>
+                        </div>
+                        <div class="modal-body">
 
-                                </div>
-                            </div>
                         </div>
                     </div>
+                </div>
+            </div>
 
-                     <!--monthly cost and expense cost -->
-                    <div class="modal fade bd-example-modal-cost costcheck" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" style="display:none;">
-                        <div class="modal-dialog modal-xl" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLongTitle">料金プラン</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <button class="btn btn-secondary">閉じる&times;</button>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    <table class="table table-bordered price_tbl">
-                                        <thead>
-                                            <tr>
-                                                <th scope="col">プラン名／居室詳細</th>
-                                                <th scope="col">入居時費用</th>
-                                                <th scope="col">月額費用</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody v-for="payment in payment_name" :key="payment.id">
-                                            <tr>
-                                                <td><h5>【増税対応済】{{payment.payment_name}}</h5>
-                                                    <p class="room_type"><span>{{payment.living_room_type}}</span>{{payment.area}}</p>
-                                                </td>
-                                                <td class="expense_txt">{{payment.expense_moving}}</td>
-                                                <td class="expense_txt">{{payment.monthly_fees}}</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
+            <!--monthly cost and expense cost -->
+            <div class="modal fade bd-example-modal-cost costcheck" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" style="display:none;">
+                <div class="modal-dialog modal-xl" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle">料金プラン</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <button class="btn btn-secondary">閉じる&times;</button>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <table class="table table-bordered price_tbl">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">プラン名／居室詳細</th>
+                                        <th scope="col">入居時費用</th>
+                                        <th scope="col">月額費用</th>
+                                    </tr>
+                                </thead>
+                                <tbody v-for="payment in payment_name" :key="payment.id">
+                                    <tr>
+                                        <td><h5>【増税対応済】{{payment.payment_name}}</h5>
+                                            <p class="room_type"><span>{{payment.living_room_type}}</span>{{payment.area}}</p>
+                                        </td>
+                                        <td class="expense_txt">{{payment.expense_moving}}</td>
+                                        <td class="expense_txt">{{payment.monthly_fees}}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
-                    <!-- end monthly cost -->
-                <div class="col-12 m-t-20"  id="fav-history-page">
-                    <div class="row justify-content-lg-center">
+                </div>
+            </div>
+            <!-- end monthly cost -->
+            <div class="col-12">
+                <div class="m-t-20"  id="fav-history-page">
+                    <div class="col-12">
                         <div class="card-carousel-wrapper">
 
                             <div class="nav-box" @click="moveCarousel(-1)" :disabled="atHeadOfList">
@@ -95,7 +97,7 @@
                             </div>
                             <div class="card-carousel">
                                 <div class="card-carousel--overflow-container">
-                                    <div class="card-carousel-cards col-3" :style="{ transform: 'translateX' + '(' + currentOffset + 'px' + ')'}">
+                                    <div class="card-carousel-cards" :style="{ transform: 'translateX' + '(' + currentOffset + 'px' + ')'}">
                                         <div class="card-carousel--card">
                                             <div class="card-carousel--card--footer">
                                                 <table class="table table-bordered">
@@ -104,8 +106,9 @@
                                                             <div class="profile_img_wrap">
                                                                 <img class="profile_img" v-bind:src="'/upload/nursing_profile/' + nur_profile.logo" alt @error="imgUrlAlt"/>
                                                             </div>
-                                                            <router-link class="pseudolink" :to="{name: 'profile', params: {cusid:nur_profile.customer_id, type: 'nursing'}}" >{{nur_profile.name}}</router-link>
-
+                                                            <div class="profile_wd">
+                                                                <router-link class="pseudolink" :to="{name: 'profile', params: {cusid:nur_profile.customer_id, type: 'nursing'}}" >{{nur_profile.name}}</router-link>
+                                                            </div>
                                                         </td>
                                                     </tr>
 
@@ -135,14 +138,14 @@
                                                     </td>
                                                 </tr>
 
-                                                    <tr>
-                                                        <td v-for="nur_profile in nur_profiles" :key="nur_profile.id">
-                                                            <dl>
-                                                                <dt class="text-left">交通アクセス</dt>
-                                                                <dd class="profile_wd"><span v-html="nur_profile.access"></span></dd>
-                                                            </dl>
-                                                        </td>
-                                                    </tr>
+                                                <tr>
+                                                    <td v-for="nur_profile in nur_profiles" :key="nur_profile.id">
+                                                        <dl>
+                                                            <dt class="text-left">交通アクセス</dt>
+                                                            <dd class="profile_wd"><span v-html="nur_profile.access"></span></dd>
+                                                        </dl>
+                                                    </td>
+                                                </tr>
 
                                                     <tr>
                                                     <td v-for="nur_profile in nur_profiles" :key="nur_profile.id">
@@ -159,7 +162,7 @@
                                                     </td>
                                                 </tr>
 
-                                                 <tr>
+                                                <tr>
                                                     <td v-for="nur_profile in nur_profiles" :key="nur_profile.id">
                                                         <dl>
                                                             <dt class="text-left">月額の費用</dt>
@@ -168,13 +171,13 @@
                                                     </td>
                                                 </tr>
 
-                                                 <tr>
+                                                <tr>
                                                     <td v-for="nur_profile in nur_profiles" :key="nur_profile.id">
                                                         <div class="profile_wd"><span class="pseudolink" @click="monthlyCost(nur_profile.id)" data-toggle="modal" data-target=".bd-example-modal-cost"><i class="fa fa-search"></i>料金プランの詳細</span></div>
                                                     </td>
                                                 </tr>
 
-                                                 <tr>
+                                                <tr>
                                                     <td v-for="nur_profile in nur_profiles" :key="nur_profile.id">
                                                         <dl>
                                                             <dt class="text-left">入居条件</dt>
@@ -183,7 +186,7 @@
                                                     </td>
                                                 </tr>
 
-                                                     <tr>
+                                                    <tr>
                                                         <td v-for="nur_profile in nur_profiles" :key="nur_profile.id">
                                                             <ul class="fac_container profile_wd">
                                                                 <h6 class="text-left font-weight-bold">特長</h6>
@@ -226,8 +229,9 @@
                         </div>
                     </div>
                 </div>
-
-  </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
